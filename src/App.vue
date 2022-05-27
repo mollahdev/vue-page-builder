997972bb1f8e3d72d24fe099f79f5877f0457982
mollahdev <mollah.dev@gmail.com>
2022-05-27 09:23:55 +0600

    added tailwind

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+import Preloader from '@/components/Preloader.vue';
+export default {
+  name: 'App',
+  components: { Preloader }
+}
+</script>
+
+
+<template>
+  
+</template>
+
---
+++ src/components/Preloader.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <div class="bg-gray-200 fixed top-0 left-0 w-full h-screen place-items-center grid">
+            <div class="bg-white">
+                <div>one</div>
+                <label>Loading</label>
+            </div>
+        </div>
+    </div>
+</template>
